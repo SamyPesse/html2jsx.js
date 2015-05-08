4d3cfaa6748f14a025478684ca53b2f63c772b76
Samy Pessé <samypesse@gmail.com>
2015-05-08 20:55:36 +0200

    Map jsx attribute names

--- test/parser.js
+++ test/parser.js
@@ -6,7 +6,7 @@ describe('HTML2Jsx', function() {
     it('should return attributes', function(done) {
         parser('<h1 foo="beep">Hello World</h1>', function(err, jsx) {
             if (err) return done(err);
-            assert.equal(jsx, 'React.createElement("h1", { "foo": "beep"}, [ "Hello World" ])', 'success')
+            assert.equal(jsx, 'React.createElement("h1", { "foo": "beep" }, [ "Hello World" ])');
             done();
         })
     });
@@ -14,7 +14,7 @@ describe('HTML2Jsx', function() {
     it('should return children', function(done) {
         parser('<p>Hello\n<b>World</b></p>', function(err, jsx) {
             if (err) return done(err);
-            assert.equal(jsx, 'React.createElement("p", null, [ "Hello\\n", React.createElement("b", null, [ "World" ]) ])', 'success')
+            assert.equal(jsx, 'React.createElement("p", null, [ "Hello\\n", React.createElement("b", null, [ "World" ]) ])');
             done();
         })
     });
@@ -22,7 +22,15 @@ describe('HTML2Jsx', function() {
     it('should handle correctly pre', function(done) {
         parser('<pre>Hello\nWorld</pre>', function(err, jsx) {
             if (err) return done(err);
-            assert.equal(jsx, 'React.createElement("pre", null, [ "Hello\\nWorld" ])', 'success')
+            assert.equal(jsx, 'React.createElement("pre", null, [ "Hello\\nWorld" ])');
+            done();
+        })
+    });
+
+    it('should use className instead of class', function(done) {
+        parser('<div class="test">Hello</div>', function(err, jsx) {
+            if (err) return done(err);
+            assert.equal(jsx, 'React.createElement("div", { "className": "test" }, [ "Hello" ])');
             done();
         })
     });
